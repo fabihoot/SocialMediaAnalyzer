75bc66798012129b94a16f0f44f4f8817f6af1ba
fabihoot <fabian.huth@stud.uni-regensburg.de>
2015-03-17 13:49:42 +0100

    added node-natural added tokenization and stemming of words

--- models/analyzer.js
+++ models/analyzer.js
@@ -1,9 +1,24 @@
 var sentiment = require('sentiment');
+var natural = require('natural');
+var tokenizer = new natural.WordTokenizer();
 
 analyzeMediaElement = function(mediaElement){
+		tokenizeText(mediaElement.text);
 		var sent = sentiment(mediaElement.text);
-		//console.log("Sentiment Score / Comparison: " + sent.score + " / " + sent.comparison);
-	
+		//console.log("Sentiment Score / Comparison: " + sent.score + " / " + sent.comparison);	
+}
+
+tokenizeText = function(text){
+	var tokens = [];
+	tokens = tokenizer.tokenize(text);
+	stemTokens(tokens);	
+}
+
+stemTokens = function(tokens){
+	for (var i = 0; i < tokens.length; i++){
+		tokens[i] = natural.PorterStemmer.stem(tokens[i]);		
+	}
+
 }
 
 exports.analyzeMediaElement = analyzeMediaElement;

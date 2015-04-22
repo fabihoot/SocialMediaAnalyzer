d129d3483aabfeb60769586bcfc8281e74e2be37
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-04-22 16:04:30 +0200

    updated layout

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -1,9 +1,24 @@
  var $btnSubmit = $('#submit-button');
  var $txtKeyword = $('#input-keyword');
+ var $resultContainer = $('#result-container');
+
+ var $panelOne = $('#panel-heading-one');
+ var $panelTwo = $('#panel-heading-two');
+ var $panelThree = $('#panel-heading-three');
+ var $panelFour = $('#panel-heading-four');
 
  $btnSubmit.click(function(){
 
    var searchTerm = $txtKeyword.val();
+   $panelOne.fadeIn(500, function(){     
+      $panelTwo.fadeIn(500, function(){
+         $panelThree.fadeIn(500, function(){
+            $panelFour.fadeIn(500, function(){
+      
+            });
+         });
+      });
+   });
    if (searchTerm == "") return;
    console.log("keyword: " + searchTerm);
 

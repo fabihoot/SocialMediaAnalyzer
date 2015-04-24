6cb45c9333e9d49484db5910f88f5daaa1586595
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-04-24 10:29:41 +0200

    added condition of no empty texts to analyzer

--- models/analyzer.js
+++ models/analyzer.js
@@ -4,22 +4,25 @@ var tokenizer = new natural.WordTokenizer();
 var analyze = require('Sentimental').analyze;
 
 analyzeMediaElement = function(mediaElement){
+	
+	if(mediaElement.text != "" && mediaElement.text != undefined){
+
 		mediaElement.lang.tokens = tokenizeText(mediaElement.text);
 		mediaElement.lang.countTokens = mediaElement.lang.tokens.length;
 		mediaElement.sentiment = sentimentAnalysis(mediaElement);
 		
-		return mediaElement;	
+	}	
+	return mediaElement;
 }
 
-sentimentAnalysis = function(mediaElement){
-
-	//Sentiment analysis with node module 'sentiment'
-	var sent = sentiment(mediaElement.text);
-	//console.log("Sentiment Score / Comparison: " + sent.score + " / " + sent.comparison);
-
-	//Sentiment analysis with node module 'Sentimental'
-	var an = analyze(mediaElement.text); 	
-	return an;
+sentimentAnalysis = function(mediaElement){	
+		//Sentiment analysis with node module 'sentiment'
+		var sent = sentiment(mediaElement.text);
+		//console.log("Sentiment Score / Comparison: " + sent.score + " / " + sent.comparison);
+	
+		//Sentiment analysis with node module 'Sentimental'
+		var an = analyze(mediaElement.text); 	
+		return an;			
 }
 
 tokenizeText = function(text){

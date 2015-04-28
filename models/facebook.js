7095077450dab3cf32565a5b509533d524b9f7bc
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-04-28 11:30:32 +0200

    updated pagination for facebook search

--- models/facebook.js
+++ models/facebook.js
@@ -5,54 +5,101 @@ var optionsgetFBFeed;
 var serializer = require('./serializer');       
 var https = require('https');
 var async = require('async');
+var nextLink = '';
 
 
-initSiteOptions = function(search_word, count){
-var c = count / 10;  
-  optionsgetFB = {
-        host :    'graph.facebook.com',
-        port :    443,
-        path :    '/search?q=' + search_word + '&type=page&limit='+ c + '&access_token=' +  my_access_token + '&locale=en_US',
-        method :  'GET'
-  };
+initSiteOptions = function(search_word, count, optionsChanged, nextLink){
+  if(!optionsChanged){
+    
+      optionsgetFB = {
+            host :    'graph.facebook.com',
+            port :    443,
+            path :    '/search?q=' + search_word + '&type=page&limit='+ count + '&access_token=' +  my_access_token + '&locale=en_US',
+            method :  'GET'
+    };
+  
+  } else {
+    optionsgetFB = {
+            host :    'graph.facebook.com',              
+            port :    443,
+            path :    getNextLink(),
+            method :  'GET'
+    };
+  }
 }
 
 initFeedOptions = function(page_id){  
   optionsgetFBFeed  = {
         host :    'graph.facebook.com',
         port :    443,
-        path:     '/'+ page_id + '/feed?limit=10&access_token=' +  my_access_token + '&locale=en_US',
+        path:     '/'+ page_id + '/feed?limit=1&access_token=' +  my_access_token,
         method :  'GET'
   };
 }
 
-getFacebookData = function(search_word, count, callback){   
-  initSiteOptions(search_word, count);
-  var reqGetFB = https.request(optionsgetFB, function(res) {
-      
-      var content = '';   
-      res.on('data', function(data) {
-                
-          content += data;   
-                   
-      });
+getFacebookData = function(search_word, count, callback){
+ 
+  var facebookFeedEntries  = {"data" : []};
+  var resultData = null;
+  var siteOptionsChanged = false;
+  
+  async.whilst(
+    function() {
+    
+        if(resultData == null){          
+           return true;
+        } else {
+          for(var i = 0;i<resultData.data.length; i++){
+            facebookFeedEntries.data.push(resultData.data[i]);
+            if (facebookFeedEntries.data.length >= count){
+              return false;
+            }
+          }                
+          siteOptionsChanged = true;               
+          return true;
+          
+        }
 
-      res.on('end', function(data) {     
-        var formatedJSON = JSON.parse(content);         
-        getFacebookFeed(formatedJSON.data, function( data ){
-          callback( data );
+    },
+    function( next ) {    
+    initSiteOptions(search_word, count, siteOptionsChanged, nextLink);
+    var reqGetFB = https.request(optionsgetFB, function(res) {
+        
+        var content = '';   
+        res.on('data', function(data) {
+                  
+            content += data;   
+                     
         });
-      });
+  
+          res.on('end', function(data) { 
 
-  });   
-   
-  reqGetFB.end();
-  reqGetFB.on('error', function(e) {
-      console.error(e);
-  });
+            var formatedJSON = JSON.parse(content);            
+            setNextLink(formatedJSON.paging.next);
+
+            getFacebookFeeds(formatedJSON.data, function( data ){
+
+              resultData = data;
+              next(null, data);
+
+            });
+          });
+  
+      });   
+       
+      reqGetFB.end();
+      reqGetFB.on('error', function(e) {
+          console.error(e);
+      });
+  
+    },
+    function( err, result ) {
+      callback( facebookFeedEntries );      
+    }
+  );
 }
 
-getFacebookFeed = function(array, callback) {
+getFacebookFeeds = function(array, callback) {
     var facebookFeedEntries  = {"data" : []};
     
     async.times(array.length, function(n, next){
@@ -61,17 +108,8 @@ getFacebookFeed = function(array, callback) {
       next( err, entry );
      })
 
-    }, function(err, entries) {
-      console.log(err);
-
-      for (var i = 0;i<entries.length;i++){
-        for(var j = 0;j<entries[i].length;j++){
-
-          facebookFeedEntries.data.push(entries[i][j]);
-          
-        }       
-      }      
-     
+    }, function(err, entries) {      
+      formatMappingResponse(entries, facebookFeedEntries);     
       callback(facebookFeedEntries);
     });
 
@@ -107,8 +145,27 @@ requestFacebookFeedEntries = function(page_id, callback){
 
     reqGetFBFeed.end();
     reqGetFBFeed.on('error', function(e) {
+      callback(e, null);      
       console.error(e);
     });
    
+}
+
+formatMappingResponse = function ( array, arrayToPush ){
+
+for (var i = 0;i<array.length;i++){
+        for(var j = 0;j<array[i].length;j++){
+
+          arrayToPush.data.push(array[i][j]);
+          
+        }       
+}
+  
+}
+setNextLink = function(link){
+  nextLink = link;
+}
+getNextLink = function(){
+  return nextLink;
 }
 exports.getFacebookData = getFacebookData;

cfde3eef06d5ac6bf9982150de4fe6ac36ea8892
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-05-21 17:53:47 +0200

    added panel animation

--- public/javascripts/views/Search.js
+++ public/javascripts/views/Search.js
@@ -1,20 +1,29 @@
 SocialMediaAnalyzer.Search = (function() {
 	var that = {},
+  $panelGeneral,
+  $panelPosts,
+  $panelMapping,
 
 	init = function() {
 		console.log("init Search.js");
     initListener();
+    $panelGeneral = $('#frame-general');
+    $panelPosts = $('#frame-posts');
+    $panelMapping = $('#frame-mapping');    
 	},
 
   initListener = function(){
     $(document).on('onShowPanels', showPanels);
   },
   
-	showPanels = function(){
+	showPanels = function(event){
+    event.preventDefault();
     console.log("show Panels");
+    $panelGeneral.removeClass('hidden').addClass('magictime spaceInLeft'); 
+    $panelPosts.removeClass('hidden').addClass('magictime spaceInLeft'); 
+    $panelMapping.removeClass('hidden').addClass('magictime spaceInLeft'); 
   };
 
-   that.init = init;
-   that.showPanels = showPanels;
+   that.init = init;   
    return that;
 }());

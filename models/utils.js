7cd8e3ba88c883f68892d7a7325d3d3e67489b67
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-05-21 14:57:54 +0200

    added appsecret get from config method

--- models/utils.js
+++ models/utils.js
@@ -1,12 +1,21 @@
 var fs = require('fs');
 
-getAppId = function (callback){
+getAppId = function (callback){	
 	fs.readFile('./config/config.json', function(err, data){
 		if (err) console.error(err);
 		var obj = JSON.parse(data);		
-		var appId = obj.facebook.appId;
+		var appId = obj.facebook.appId;		
 		callback(appId);
+	});
+}	
+getAppSecret = function (callback){	
+	fs.readFile('./config/config.json', function(err, data){
+		if (err) console.error(err);
+		var obj = JSON.parse(data);		
+		var appSecret = obj.facebook.appSecret;		
+		callback(appSecret);
 	});
 }
 //exports.method = method;
 exports.getAppId = getAppId;
+exports.getAppSecret = getAppSecret;

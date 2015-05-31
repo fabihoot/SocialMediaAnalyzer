2dd548f8cbeb557065109915e2e7eebe9d1a2e57
fabihoot <fabian.huth@stud.uni-regensburg.de>
2015-05-31 11:47:55 +0200

    updated visualisation

--- public/javascripts/views/Visualization.js
+++ public/javascripts/views/Visualization.js
@@ -28,11 +28,11 @@ SocialMediaAnalyzer.Visualization = (function() {
 	createVoteBarChart = function(dataset){
     
     var charts = ['#vote-chart-fb', '#vote-chart-twitter', '#vote-chart-reddit'];
-    var width = 100,
-        height = 350;
+    var colorClasses = ['.facebook', '.twitter', '.reddit'];
+    var width = 100, height = 350;
     var barWidth = 75;
-
-    var xScale = d3.scale.linear().domain([0, d3.max(dataset)]) // your data minimum and maximum
+    var duration = 2000;
+    var xScale = d3.scale.linear().domain([0, d3.max(dataset)])
                                   .range([0, height]);
     var yScale = d3.scale.linear().domain([0, d3.max(dataset, function(d) { return d; })]).rangeRound([0, height]);
    
@@ -40,42 +40,55 @@ SocialMediaAnalyzer.Visualization = (function() {
 
     var chart = d3.select(charts[i])
                  .attr("width", width)
-                 .attr("height", height);
-      
+                 .attr("height", height);      
 		
-     chart.selectAll("svg")
+    chart.selectAll("svg")
             .data([dataset[i]]) 
           .enter().append("svg:rect")
+            .attr("height", function(d) { return yScale(d); })
             .attr("x", function(d, i) { return xScale(i); })
-            .attr("y", function(d) { return height - yScale(d); })
+            .attr("y", 350)
             .attr("width", barWidth)
-            .attr("fill", "#2d578b")
-            .text(function(d) { return d; })
-            .attr("fill", "black")
-             .transition()
-             .duration(5000)
-             .attr("height", function(d) { return yScale(d); });
+            .attr("class", colorClasses[i])
+            .transition()
+              .attr("y", function(d) { return height - yScale(d); })
+              .duration(duration)
+              .delay(1500)
+              .ease("linear");                                             
+
+    $(".vote-chart > rect").mouseenter(function() {     
+      d3.select(this).transition()
+                     .attr("width", function(){ return barWidth + 10; })
+                     .attr("x", function(d, i) { return xScale(i) - 5 ; })
+                     .duration(500);     
+    });
+     $(".vote-chart > rect").mouseleave(function() {
+      
+      d3.select(this).transition()
+                     .attr("width", function(){ return barWidth - 10; })
+                     .attr("x", function(d, i) { return xScale(i) + 5 ; })
+                     .duration(500);
+    });
     chart.selectAll("text")
               .data([dataset[i]])
             .enter().append("svg:text")
               .attr("x", function(d, i) { return xScale(i) + barWidth; })
               .attr("y", function(d) {
                 var yValue = height - yScale(d);
-                  if(yValue >= 460){
-                    return 460;
+                  if(yValue >= 300){
+                    return 300;
                   } else {
                   return yValue                  
-                  }
+                  }                  
                  })
               .attr("dx", - barWidth/2)
               .attr("dy", "1.2em")
               .attr("text-anchor", "middle")
               .text(function(d) { return d; })
-              .attr("fill", "black");
+              .attr("fill", "white");
   
          
     }
-    
 
 	};
 

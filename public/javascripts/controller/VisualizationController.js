ff18fa2ba26a16e00c9b2a810380e18e22b02b11
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-06-01 21:51:29 +0200

    changed tile creation

--- public/javascripts/controller/VisualizationController.js
+++ public/javascripts/controller/VisualizationController.js
@@ -35,23 +35,96 @@ redditData = null,
  	SocialMediaAnalyzer.Visualization.createVoteBarChart([fbVotes, twitterVotes, redditVotes]);
  },
 
- setTwitterPosts = function(){        
+ setTwitterPosts = function(){      
       $( '#twitter-post-result' ).render( twitterData, getDirective() );
   },
 
-  setRedditPosts = function(){               
-      $( '#reddit-post-result' ).render( redditData, getDirective() );
+  setRedditPosts = function(){
+      createTiles(redditData);               
+      //$( '#reddit-post-result' ).render( redditData, getDirectiveText() );
   },
 
   setFacebookPosts = function(){         
       $( '#facebook-post-result' ).render( facebookData, getDirective() );
   },
 
+  getDirectiveImage = function(){
+    directive = {
+         '.tile-content':{
+           'content<-':{
+              '.tile-image':function(a){
+                console.log("A:", a);
+                console.log("this", this);
+                 $('.tile-image').css('background-image', this.content.url);
+                  return this.text;                  
+                }
+              }
+            }
+          
+      };
+      return directive; 
+  },
+
+  createTiles = function(data){
+    addTiles(data);
+    addText(data);
+  },
+
+  addTiles = function(data){
+    for (var i = 0; i<data.length;i++){
+      var $tile = '<div class="tile" data-role="tile">' + 
+                   '<div class="tile-content slide-up">' + 
+                    '<div class="slide">' + 
+                       '<div class="tile-image"><img id="tile-img-entry-' + i + '"></img></div>' + 
+                    '</div>' + 
+                    '<div class="slide-over">' + 
+                       '<div class="tile-text" id="tile-text-entry-'+ i + '"></div>' + 
+                    '</div>' + 
+                   '</div>' + 
+                 '</div>';
+      $( '#reddit-post-result' ).append($tile);
+      var text = data[i].text;
+      var url = data[i].content.url;    
+
+      var $txtContainer = $('#tile-text-entry-'+ i);
+      var $imgContainer = $('#tile-img-entry-' + i);
+      console.log(url);
+      $txtContainer.text(text);
+      $imgContainer.attr("src",  url);  
+    }    
+  },
+
+  addText = function(data){
+   
+  },
+
+  getDirectiveText = function(){
+    directive = {
+       '.tile':{
+        'content<-':{
+         '.slide-over':{
+           'content<-':{
+              '.tile-text':function(a){              	
+                if(this.text.length > 100){
+                  var temp = this.text.substring(0, 100);
+                  temp = temp + "...";
+                  return temp;
+                } else {
+                  return this.text;                  
+                }
+              }
+         }
+        }
+        }
+      }
+      };
+      return directive; 
+  },
   getDirective = function(){
     directive = {
          '.tile':{
            'content<-':{
-              '.tile-content':function(a){              	
+              '.tile-content':function(a){                
                 if(this.text.length > 100){
                   var temp = this.text.substring(0, 100);
                   temp = temp + "...";
@@ -64,7 +137,8 @@ redditData = null,
           }
       };
       return directive; 
-  };
+  };;
+
 
 that.createVoteVisualization = createVoteVisualization;
 that.setFacebookData = setFacebookData;

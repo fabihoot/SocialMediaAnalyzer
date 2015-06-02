576f4811a68b91a24da2b57dc05bb9899a6322cf
Fabian Huth <fabian.huth@stud.uni-regensburg.de>
2015-06-02 13:01:04 +0200

    added hashtagset to twitter elements

--- models/analyzer.js
+++ models/analyzer.js
@@ -10,14 +10,17 @@ analyzeMediaElement = function(mediaElement){
 
 		//console.log("Source is " + mediaElement.source);		
 		//console.log("Text is " + mediaElement.text);
-		mediaElement.lang.probLang = checkLanguage(mediaElement.text);
 		//console.log("Language is " + mediaElement.lang.probLang);
 		//console.log("Type is " + mediaElement.type);
+		mediaElement.lang.probLang = checkLanguage(mediaElement.text);
 		mediaElement.lang.tokens = tokenizeText(mediaElement.text);
 		mediaElement.lang.countTokens = mediaElement.lang.tokens.length;		
-		mediaElement.sentiment = sentimentAnalysis(mediaElement);
+		mediaElement.sentiment = sentimentAnalysis(mediaElement);		
 		if(mediaElement.lang.probLang != 'english'){			
 			return null;
+		}
+		if(mediaElement.source != 'twitter'){
+			mediaElement.lang.hashtags = getHashtags(mediaElement.text);
 		} 		
 	} else {
 		return null;
@@ -46,7 +49,6 @@ stemTokens = function(tokens){
 	for (var i = 0; i < tokens.length; i++){
 		tokens[i] = natural.PorterStemmer.stem(tokens[i]);		
 	}
-
 }
 
 checkLanguage = function(text){	
@@ -56,6 +58,18 @@ checkLanguage = function(text){
 	} else {
 		return "Could not identify language";
 	}
+}
+
+getHashtags = function(str){
+	var text = str.replace(/(\r\n|\n|\r)/gm,"");
+	var tagslistarr = text.split(' ');
+	var arr = [];
+	for(var i = 0; i<tagslistarr.length; i++){
+		if(tagslistarr[i].indexOf('#') == 0){
+    	  arr.push(tagslistarr[i]);  
+    	}    			
+	}
+	return arr;
 }
 
 exports.analyzeMediaElement = analyzeMediaElement;
